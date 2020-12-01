a73e480088b60b019776f11bcd85a1386952ee02
Gerico Vidanes <gericov@outlook.com>
2020-12-01 17:38:15 +0000

    Re structured testing folder

--- testcases/Vel_Profiles/Inviscid-Incompressible/readme.txt
+++ testcases/Vel_Profiles/Inviscid-Incompressible/readme.txt
@@ -1,2 +1,3 @@
 Replicating test cases from K.Smith's StreamVane paper with swirlgenerator.
 Basic inviscid, incompressible cases
+Includes .cfg file for running SU2
--- testcases/Vel_Profiles/Inviscid-Incompressible/recreate_testcases.bat
+++
@@ -1,5 +0,0 @@
-python ..\..\..\swirlgenerator\Main.py bulk_swirl.config -saveplots -checkboundaries -makemesh -showmesh
-python ..\..\..\swirlgenerator\Main.py twin_swirl.config -saveplots -checkboundaries -makemesh -showmesh
-python ..\..\..\swirlgenerator\Main.py offset_swirl_1.config -saveplots -checkboundaries
-python ..\..\..\swirlgenerator\Main.py offset_swirl_2.config -saveplots -checkboundaries
-PAUSE
