f82772fbc4962604356f076b5b5957b9e5c830a1
Gerico Vidanes <gericov@outlook.com>
2021-05-17 15:12:37 +0100

    Added a simple python file to showcase the API like interface to the tool - instead of using the command line/config file interface. Does both the vortex and contour translation methods and saved the plots to a pdf. Also shows how to do contour translation with non-default parameters.

---
+++ swirlgenerator/showcase.py
@@ -0,0 +1,86 @@
+import core
+import contour_translation as ct
+import pre
+import post
+import writeBC
+
+import os
+import time
+
+# Get the path the repo has been cloned to
+scriptpath = os.path.split(os.path.realpath(__file__))[0]
+parentpath = os.path.dirname(scriptpath)
+
+# Mesh file path
+meshfile = os.path.join(parentpath, 'cylinder.su2')
+
+# Extract nodes from mesh
+nodes = pre.Input.extractMesh(meshfile)
+
+# 
+# Vortex method
+#
+print('Doing vortex method...')
+start = time.time()
+
+# Initialise FlowField object with the nodes
+flowfield_vm = core.FlowField(nodes)
+
+# Specify the individual vortices to be placed in the domain
+vortexDefs = core.Vortices(model='lo', centres=[[0.083, 0.0], [-0.083, 0.0]], strengths = [1.243, -1.243], radius=[0.25,0.25])
+# Place vortices into domain and calculate velocity effects
+flowfield_vm.computeDomain(vortexDefs, axialVel=1)
+
+# Write boundary condition file
+writeBC.writeSU2(flowfield_vm, 'twinvortex_VM.dat')
+
+print(f'Elapsed time: {time.time()-start}s\n')
+
+# Visualise flowfield and save to pdf
+print('Making plots...')
+start = time.time()
+
+plots = post.Plots(flowfield_vm)
+plots.plotAll(pdfName='twinvortex_VM.pdf')
+
+print(f'Elapsed time: {time.time()-start}s\n')
+
+
+# 
+# Contour Translation Method
+#
+print('Doing contour translation method...')
+start = time.time()
+
+# Initialise FlowField object with the nodes
+flowfield_ctm = core.FlowField(nodes)
+
+# Path to images
+tangentialImage = os.path.join(parentpath, 'Testing', 'IdealisedSwirlCases', 'twinswirl_tangential.PNG')
+radialImage = os.path.join(parentpath, 'Testing', 'IdealisedSwirlCases', 'twinswirl_radial.PNG')
+
+# Initialise contour translation objects but don't translate yet
+tangential = ct.Contour(tangentialImage, [-20,20], doTranslation=False)
+radial = ct.Contour(radialImage, [-20,20], doTranslation=False)
+
+# Translate contour plots (with some given parameters) to data
+tangential.translateContourPlot(samplingMode=1, samplingParams=(10), circleParams=(200,30), minLevels=100, shrinkPlotMax=10)
+radial.translateContourPlot(samplingMode=1, samplingParams=(10), circleParams=(200,30), minLevels=100, shrinkPlotMax=10)
+
+# Reconstruct flow field from contour plot data
+flowfield_ctm.reconstructDomain(tangential.getValuesAtNodes(flowfield_ctm.coords), \
+                                radial.getValuesAtNodes(flowfield_ctm.coords), axialVel=1)
+
+# Write boundary condition file
+writeBC.writeSU2(flowfield_ctm, 'twinvortex_ctm.dat')
+
+print(f'Elapsed time: {time.time()-start}s\n')
+
+# Visualise flowfield and save to pdf
+print('Making plots...')
+start = time.time()
+
+plots = post.Plots(flowfield_ctm)
+plots.plotAll(pdfName='twinvortex_ctm.pdf')
+
+print(f'Elapsed time: {time.time()-start}s\n')
